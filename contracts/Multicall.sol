de8abd6c42d9075bcc8bb71135b0fe107863fe97
Heesho <hisham11027@gmail.com>
2025-11-03 15:21:42 -0800

    built out dutch auction for burning LP tokens

--- contracts/Multicall.sol
+++ contracts/Multicall.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IWETH {
     function deposit() external payable;
@@ -34,12 +35,34 @@ interface IMiner {
     ) external returns (uint256 price);
 }
 
-contract Multicall {
+interface IAuction {
+    struct Slot0 {
+        uint8 locked;
+        uint16 epochId;
+        uint192 initPrice;
+        uint40 startTime;
+    }
+
+    function paymentToken() external view returns (address);
+    function getPrice() external view returns (uint256);
+    function getSlot0() external view returns (Slot0 memory);
+    function buy(
+        address[] calldata assets,
+        address assetsReceiver,
+        uint256 epochId,
+        uint256 deadline,
+        uint256 maxPaymentTokenAmount
+    ) external;
+}
+
+contract Multicall is Ownable {
     using SafeERC20 for IERC20;
 
     address public immutable miner;
     address public immutable donut;
     address public immutable quote;
+
+    address public auction;
 
     struct MinerState {
         uint16 epochId;
@@ -54,6 +77,16 @@ contract Multicall {
         uint256 ethBalance;
         uint256 wethBalance;
         uint256 donutBalance;
+    }
+
+    struct AuctionState {
+        uint16 epochId;
+        uint192 initPrice;
+        uint40 startTime;
+        address paymentToken;
+        uint256 price;
+        uint256 wethBalance;
+        uint256 paymentTokenBalance;
     }
 
     constructor(address _miner) {
@@ -74,6 +107,22 @@ contract Multicall {
         IERC20(quote).safeTransfer(msg.sender, wethBalance);
     }
 
+    function buy(uint256 epochId, uint256 deadline, uint256 maxPaymentTokenAmount) external {
+        address paymentToken = IAuction(auction).paymentToken();
+        uint256 price = IAuction(auction).getPrice();
+        address[] memory assets = new address[](1);
+        assets[0] = quote;
+
+        IERC20(paymentToken).safeTransferFrom(msg.sender, address(this), price);
+        IERC20(paymentToken).safeApprove(auction, 0);
+        IERC20(paymentToken).safeApprove(auction, price);
+        IAuction(auction).buy(assets, msg.sender, epochId, deadline, maxPaymentTokenAmount);
+    }
+
+    function setAuction(address _auction) external onlyOwner {
+        auction = _auction;
+    }
+
     function getMiner(address account) external view returns (MinerState memory state) {
         IMiner.Slot0 memory slot0 = IMiner(miner).getSlot0();
         state.epochId = slot0.epochId;
@@ -88,6 +137,18 @@ contract Multicall {
         state.ethBalance = account == address(0) ? 0 : account.balance;
         state.wethBalance = account == address(0) ? 0 : IERC20(quote).balanceOf(account);
         state.donutBalance = account == address(0) ? 0 : IERC20(donut).balanceOf(account);
+        return state;
+    }
+
+    function getAuction(address account) external view returns (AuctionState memory state) {
+        IAuction.Slot0 memory slot0 = IAuction(auction).getSlot0();
+        state.epochId = slot0.epochId;
+        state.initPrice = slot0.initPrice;
+        state.startTime = slot0.startTime;
+        state.paymentToken = IAuction(auction).paymentToken();
+        state.price = IAuction(auction).getPrice();
+        state.wethBalance = IERC20(quote).balanceOf(auction);
+        state.paymentTokenBalance = account == address(0) ? 0 : IERC20(state.paymentToken).balanceOf(auction);
         return state;
     }
 }
--- tests/test0.js
+++ tests/test0.js
@@ -5,9 +5,11 @@ const { ethers, network } = require("hardhat");
 const { execPath } = require("process");
 
 const AddressZero = "0x0000000000000000000000000000000000000000";
+const AddressDead = "0x000000000000000000000000000000000000dEaD";
 
 let owner, multisig, treasury, user0, user1, user2, user3;
-let weth, miner, multicall;
+let weth, donut, miner, multicall;
+let auction0, auction1;
 
 describe("local: test0", function () {
   before("Initial set up", async function () {
@@ -24,12 +26,50 @@ describe("local: test0", function () {
     miner = await minerArtifact.deploy(weth.address, treasury.address);
     console.log("- Miner Initialized");
 
+    donut = await ethers.getContractAt(
+      "contracts/Miner.sol:Donut",
+      await miner.donut()
+    );
+    console.log("- DONUT Initialized");
+
+    const auctionArtifact = await ethers.getContractFactory("Auction");
+    auction0 = await auctionArtifact.deploy(
+      convert("0.001", 18),
+      await miner.donut(),
+      AddressZero,
+      604800,
+      convert("1.2", 18),
+      convert("0.001", 18)
+    );
+    console.log("- Auction0 Initialized");
+    auction1 = await auctionArtifact.deploy(
+      convert("0.001", 18),
+      await miner.donut(),
+      AddressDead,
+      604800,
+      convert("1.2", 18),
+      convert("0.001", 18)
+    );
+    console.log("- Auction1 Initialized");
+
     const multicallArtifact = await ethers.getContractFactory("Multicall");
     multicall = await multicallArtifact.deploy(miner.address);
     console.log("- Multicall Initialized");
 
+    await multicall.transferOwnership(multisig.address);
+    console.log("- ownership transferred to multisig");
+
+    await multicall.connect(multisig).setAuction(auction0.address);
+    console.log("- auction0 set to multicall");
+
     console.log("Initialization Complete");
     console.log();
+  });
+
+  it("transfer ownership to multisig", async function () {
+    console.log("******************************************************");
+    await miner.transferOwnership(multisig.address);
+    console.log("- ownership transferred to multisig");
   });
 
   it("Miner state", async function () {
@@ -137,6 +177,12 @@ describe("local: test0", function () {
     );
   });
 
+  it("Set treasury to auction", async function () {
+    console.log("******************************************************");
+    await miner.connect(multisig).setTreasury(auction0.address);
+    console.log("- treasury set to auction0");
+  });
+
   it("User0 mines", async function () {
     console.log("******************************************************");
     let res = await multicall.getMiner(AddressZero);
@@ -223,7 +269,7 @@ describe("local: test0", function () {
         }
       );
     res = await multicall.getMiner(AddressZero);
-    await expect(res.miner).to.equal(user0.address);
+    expect(res.miner).to.equal(user0.address);
   });
 
   it("Miner state", async function () {
@@ -281,7 +327,7 @@ describe("local: test0", function () {
         }
       );
     res = await multicall.getMiner(AddressZero);
-    await expect(res.miner).to.equal(user1.address);
+    expect(res.miner).to.equal(user1.address);
   });
 
   it("Miner state", async function () {
@@ -323,7 +369,7 @@ describe("local: test0", function () {
         }
       );
     res = await multicall.getMiner(AddressZero);
-    await expect(res.miner).to.equal(user2.address);
+    expect(res.miner).to.equal(user2.address);
   });
 
   it("Miner state", async function () {
@@ -381,7 +427,7 @@ describe("local: test0", function () {
         }
       );
     res = await multicall.getMiner(AddressZero);
-    await expect(res.miner).to.equal(user0.address);
+    expect(res.miner).to.equal(user0.address);
   });
 
   it("Miner state", async function () {
@@ -398,5 +444,150 @@ describe("local: test0", function () {
       "Treasury ETH balance: ",
       divDec(minerStateTreasury.ethBalance)
     );
+  });
+
+  it("buy from auction", async function () {
+    console.log("******************************************************");
+    let res = await multicall.getAuction(user0.address);
+    console.log(res);
+    await multicall.connect(user0).buy(res.epochId, 1861439882, 0);
+  });
+
+  it("Auction state", async function () {
+    console.log("******************************************************");
+    let res = await multicall.getAuction(AddressZero);
+    console.log(res);
+  });
+
+  it("User0 mines", async function () {
+    console.log("******************************************************");
+    let res = await multicall.getMiner(AddressZero);
+    await multicall
+      .connect(user0)
+      .mine(
+        user1.address,
+        res.epochId,
+        1861439882,
+        res.price,
+        "https://example.com",
+        {
+          value: res.price,
+        }
+      );
+    res = await multicall.getMiner(AddressZero);
+    expect(res.miner).to.equal(user0.address);
+  });
+
+  it("Auction state", async function () {
+    console.log("******************************************************");
+    let res = await multicall.getAuction(AddressZero);
+    console.log(res);
+  });
+
+  it("buy from auction", async function () {
+    console.log("******************************************************");
+    let res = await multicall.getAuction(user0.address);
+    console.log(res);
+    await donut.connect(user0).approve(multicall.address, res.price);
+    await expect(
+      multicall.connect(user0).buy(res.epochId, 1861439882, res.price)
+    ).to.be.reverted;
+  });
+
+  it("Set treasury to auction", async function () {
+    console.log("******************************************************");
+    await miner.connect(multisig).setTreasury(auction1.address);
+    await multicall.connect(multisig).setAuction(auction1.address);
+    console.log("- auction set to auction1");
+  });
+
+  it("User0 mines", async function () {
+    console.log("******************************************************");
+    let res = await multicall.getMiner(AddressZero);
+    await multicall
+      .connect(user0)
+      .mine(
+        user1.address,
+        res.epochId,
+        1861439882,
+        res.price,
+        "https://example.com",
+        {
+          value: res.price,
+        }
+      );
+    res = await multicall.getMiner(AddressZero);
+    expect(res.miner).to.equal(user0.address);
+  });
+
+  it("Auction state", async function () {
+    console.log("******************************************************");
+    let res = await multicall.getAuction(AddressZero);
+    console.log(res);
+  });
+
+  it("buy from auction", async function () {
+    console.log("******************************************************");
+    let res = await multicall.getAuction(user0.address);
+    console.log(res);
+    await multicall.connect(user0).buy(res.epochId, 1861439882, 0);
+  });
+
+  it("Auction state", async function () {
+    console.log("******************************************************");
+    let res = await multicall.getAuction(AddressZero);
+    console.log(res);
+  });
+
+  it("User0 mines", async function () {
+    console.log("******************************************************");
+    let res = await multicall.getMiner(AddressZero);
+    await multicall
+      .connect(user0)
+      .mine(
+        user1.address,
+        res.epochId,
+        1861439882,
+        res.price,
+        "https://example.com",
+        {
+          value: res.price,
+        }
+      );
+    res = await multicall.getMiner(AddressZero);
+    expect(res.miner).to.equal(user0.address);
+  });
+
+  it("Auction state", async function () {
+    console.log("******************************************************");
+    let res = await multicall.getAuction(AddressZero);
+    console.log(res);
+  });
+
+  it("buy from auction", async function () {
+    console.log("******************************************************");
+    let res = await multicall.getAuction(user3.address);
+    console.log(res);
+    await donut.connect(user0).transfer(user3.address, res.price);
+    await donut.connect(user3).approve(multicall.address, res.price);
+    await multicall.connect(user3).buy(res.epochId, 1861439882, res.price);
+  });
+
+  it("Auction state", async function () {
+    console.log("******************************************************");
+    let res = await multicall.getAuction(AddressZero);
+    console.log(res);
+  });
+
+  it("Donut balance in Dead Address", async function () {
+    console.log("******************************************************");
+    let res = await donut.balanceOf(AddressDead);
+    console.log("Donut balance in Dead Address: ", divDec(res));
+  });
+
+  it("WETH balance in User3 Address", async function () {
+    console.log("******************************************************");
+    let res = await weth.balanceOf(user3.address);
+    console.log("WETH balance in User3 Address: ", divDec(res));
   });
 });
